6bb72cf1841c6821e53483a3db2d5d2d72bf94c8
Mahesh Narasimhan <mnarasimhan02@users.noreply.github.com>
2017-04-20 16:42:15 -0400

    updated knockout,added foresquare API

--- js/script.js
+++ js/script.js
@@ -1,123 +1,216 @@
+function viewModel() {
+  var self = this; 
+  var map, service, infowindow, address;
+  var markers = []; 
+  var myLocation = {}; 
 
-var map;
-function initialize() {
-  var mapOptions = {
-    zoom: 15,
-    center: new google.maps.LatLng(38.893952, -77.029613)
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+  // center map:  City Neighborhood in Maryland
+  var maryland = new google.maps.LatLng(39.136385, -77.216324);  
 
-  setMarkers(mapModel.markers);
-}
+  // holds places array, infowindow, markers, and all other info
+  self.placeArray = ko.observableArray([]);
 
-function loadScript() {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' +
-      '&signed_in=true&callback=initialize';
-  document.body.appendChild(script);
-}
-window.onload = loadScript;
+  // string to hold foursquare information
+  self.fourSquareAPI = '';
+  
+  // load the map
+  function init() {
+    map = new google.maps.Map(document.getElementById('map'), {
+    center: maryland,
+    zoom: 16,
+    mapTypeId: google.maps.MapTypeId.ROADMAP    
+    });
+    // search for parks within radius 
+    var request = {
+      location: maryland,
+      radius: 900,
+      types: ['park']
+    };
+
+    // infowindow to display content
+    infowindow = new google.maps.InfoWindow();
+    
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);         
+
+    // will show a list of all the markers
+    var list = (document.getElementById('list'));
 
 
-var mapModel = {
-    markers: [
-        {   
-        title: "Thomas Jefferson Memorial",
-        lat: 38.881004, 
-        lng: -77.036463,
-        map: map,
-        contentString: "<p>Blah blah blah blah blah</p>"
-        },
-        {   
-        title: "Lincoln Memorial",
-        lat: 38.889269, 
-        lng: -77.050176,
-        map: map,
-        },
-        {   
-        title: "Washington Monument",
-        lat: 38.889484, 
-        lng: -77.0363733,
-        map: map,
-        },
-        {   
-        title: "United States Capital",
-        lat: 38.889939, 
-        lng: -77.00905,
-        map: map,
-        },
-        {
-        title: "United States White House",
-        lat: 38.8989013, 
-        lng: -77.0324048,
-        map: map,
-        },
-        {   
-        title: "National WWII Memorial",
-        lat: 38.889443, 
-        lng: -77.040556,
-        map: map,
-        }   
-    ]
-}
+    map.controls[google.maps.ControlPosition.RIGHT_CENTER].push(list);
+    
+    var input = (document.getElementById('input'));
+    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(input);
+    var searchBox = new google.maps.places.SearchBox((input));
+    
+    google.maps.event.addListener(searchBox, 'places_changed', function() {
+      var places = searchBox.getPlaces();
+      clearMarkers();
+      self.placeArray.removeAll();
+      var bounds = new google.maps.LatLngBounds();  
 
-function setMarkers(location) {
-    for(i=0; i<location.length; i++) {
-        var marker = new google.maps.Marker({
-          position: new google.maps.LatLng(location[i].lat, location[i].lng),
-          map: map,
-          title: location[i].title  
-        });
-        var infowindow = new google.maps.InfoWindow({
-            content: mapModel.markers[i].contentString
-        });
+      for(var i = 0, place; i <= 10; i++){
+        if (places[i] !== undefined){
+          place = places[i];
+          allLocations(place);
+          setMarker(place);
+          bounds.extend(place.geometry.location);          
+        }
+      } 
+      map.fitBounds(bounds); 
 
-        new google.maps.event.addListener(marker, 'click', function() {
-            infowindow.open(map,marker);
-        });
+    });
+    google.maps.event.addListener(map, 'bounds_changed', function(){
+      var bounds = map.getBounds();
+      searchBox.setBounds(bounds);
+    });  
+    
+  }
+  
+  // create markers
+  function setMarker(place) {
+    var img = 'img/park.png'; 
+    var marker = new google.maps.Marker({
+      map: map,
+      name: place.name,
+      position: place.geometry.location,
+      place_id: place.place_id,
+      animation: google.maps.Animation.DROP,
+      icon:img 
+    });    
+
+    if (place.vicinity !== undefined) {
+      address = place.vicinity;
+    } else if (place.formatted_address !== undefined) {
+      address = place.formatted_address;
+    }       
+    var contentString = '<div style="font-weight: 300">' + place.name + '</div><div>' + address + '</div>' + self.fourSquareAPI;
+
+    google.maps.event.addListener(marker, 'click', function() {      
+      infowindow.setContent(contentString);      
+      infowindow.open(map, this);
+      map.panTo(marker.position); 
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+      setTimeout(function(){marker.setAnimation(null);
+    }, 1450);
+    
+    });
+
+    markers.push(marker);
+    return marker;
+  }
+
+
+  // callback function to handle results object and PlacesServiceStatus response
+  function callback(results, status){
+    if (status == google.maps.places.PlacesServiceStatus.OK){
+      bounds = new google.maps.LatLngBounds();
+      results.forEach(function (place){
+        place.marker = setMarker(place);
+        bounds.extend(new google.maps.LatLng(
+          place.geometry.location.lat(),
+          place.geometry.location.lng()));
+      });
+      map.fitBounds(bounds);
+      results.forEach(allLocations);                 
     }
-};
+  }
 
-// function infoWindow(location) {
-//     for(i=0; i<location.length; i++) {
-//         var marker = new google.maps.Marker({
-//           position: new google.maps.LatLng(location[i].lat, location[i].lng),
-//           map: map,
-//           title: location[i].title  
-//         });
-//     }
-// };
-// function setMarkers (map, location) {
-//     var image = {
-//         url: "img/location-marker.png",
-//         size: new google.maps.Size(20, 32),
-//         origin: new google.maps.Point(0,0),
-//         anchor: new google.maps.Point(0, 32)
-//     };
+  this.getFoursquareInfo = function(point) {
+    // foursquare api url
+    var foursquare = 'https://api.foursquare.com/v2/venues/search?oauth_token=COVMN3WLIESURUQOG3055SSHDF1X1MNENCNH3IFW5DJA4NBS&v=20170420&ll=39.136385,-77.216324&query=\'' + point['name'] + '\'&limit=10';
 
-//     var shape = {
-//       coords: [1, 1, 1, 20, 18, 20, 18 , 1],
-//       type: 'poly'
-//   };
+    https://api.foursquare.com/v2/venues/search?ll=39.136385, -77.216324 &query=park&oauth_token=COVMN3WLIESURUQOG3055SSHDF1X1MNENCNH3IFW5DJA4NBS&v=20170420
 
-// for (var i = 0; i < location.length; i++) {
-//     var loc = location[i];
-//     var myLatLng = new google.maps.LatLng(38.8871923, -77.0213326);
-//     var marker = new google.maps.Marker({
-//         position: myLatLng,
-//         map: map,
-//         icon: image,
-//         shape: shape,
-//         title: mapModel.markers[i].title
-//     });
-// }
-// };
+    // start ajax and grab: venue name, phone number and twitter handle
+    $.getJSON(foursquare).done(function(response) {
+        self.fourSquareAPI = '<br>' + 'Foursquare Information::' + '<br>';
+        var venue = response.response.venues[0];             
+        var venueName = venue.name;
 
+        if (venueName !== null && venueName !== undefined) {
+          self.fourSquareAPI += 'Name: ' + venueName + '<br>';
+        } else {
+          self.fourSquareAPI += venue.name;
+        }    
+        
+        var phoneNumber = venue.contact.formattedPhone;
+          if (phoneNumber !== null && phoneNumber !== undefined) {
+            self.fourSquareAPI += 'Phone: ' + phoneNumber + ' ';
+          } else {
+            self.fourSquareAPI += 'Phone not available' + ' ';
+          }
 
+        var twitterHandle = venue.contact.twitter;
+        if (twitterHandle !== null && twitterHandle !== undefined) {
+          self.fourSquareAPI += '<br>' + 'twitter: @' + twitterHandle;
+          }
+      }).fail(function(jqxhr, textStatus, error){
+        var err = textStatus + ", " + error;
+        console.log("Request Failed:" + err);  
+      });
+  };  
+ 
+  // open infowindow upon click
+  self.clickMarker = function(place) {
+    var marker;
+    for(var i = 0; i < markers.length; i++) {      
+      if(place.place_id === markers[i].place_id) { 
+        marker = markers[i];
+      }
+    } 
+    self.getFoursquareInfo(place);         
+    map.panTo(marker.position);   
 
+    // allows getFoursquareInfo function to load first
+    setTimeout(function() {
+      var contentString = '<div style="font-weight: 300">' + place.name + '</div><div>' + place.address + '</div>' + self.fourSquareAPI;
+      infowindow.setContent(contentString);
+      infowindow.open(map, marker); 
+      marker.setAnimation(google.maps.Animation.DROP); 
+    }, 300);     
+  };
 
+  // into knockout
+  function allLocations(place){
+    var myLocation = {};    
+    myLocation.place_id = place.place_id;
+    myLocation.position = place.geometry.location.toString();
+    myLocation.name = place.name;
 
-// google.maps.event.addDomListener(window, 'load', initialize);
+    if (typeof(place.vicinity) !== undefined) {
+      address = place.vicinity;
+    } else if (typeof(place.formatted_address) !== undefined) {
+      address = place.formatted_address;
+    }
+    myLocation.address = address;
+    
+    self.placeArray.push(myLocation);                
+  }
 
+  // clears marks upon picking a particular location on the autocomplete dropdown
+  function clearMarkers() {
+    for (var i = 0; i < markers.length; i++ ) {
+      if (markers[i]) {
+        markers[i].setMap(null);
+      }
+    }
+
+    // reset markers
+    markers = []; 
+  } 
+
+  // map bounds get updated as page is resized
+  google.maps.event.addDomListener(window, 'resize', function(){
+    map.setCenter(maryland); 
+  }); 
+
+  // load the map in the window
+  google.maps.event.addDomListener(window, 'load', init);
+  
+
+}
+
+$(function(){
+  ko.applyBindings(new viewModel());
+});
